d6798b73f9f0cfee11bc0805162ec4b72cd8e9a9
Johan Larsson <johan.t.larsson@gmail.com>
2021-04-06 00:09:03 +0200

    Enemy uses facing.

--- src/enemy_ai_system.rs
+++ src/enemy_ai_system.rs
@@ -1,5 +1,5 @@
 use specs::prelude::*;
-use super::{Viewshed, Position, Map, Enemy, RunState};
+use super::{Viewshed, Position, Map, Enemy, RunState, Facing, Direction, Renderable};
 use rltk::{Point};
 
 pub struct EnemyAI {}
@@ -10,10 +10,12 @@ impl<'a> System<'a> for EnemyAI {
                         ReadExpect<'a, RunState>,
                         WriteStorage<'a, Viewshed>,
                         ReadStorage<'a, Enemy>,
-                        WriteStorage<'a, Position>);
+                        WriteStorage<'a, Position>,
+                        WriteStorage<'a, Facing>,
+                        WriteStorage<'a, Renderable>);
 
     fn run(&mut self, data: Self::SystemData) {
-        let (mut map, player_entity, run_state, mut viewsheds, enemies, mut positions) = data;
+        let (mut map, player_entity, run_state, mut viewsheds, enemies, mut positions, mut facings, mut renderables) = data;
 
         if *run_state != RunState::EnemyTurn {
             return;
@@ -25,7 +27,8 @@ impl<'a> System<'a> for EnemyAI {
             player_pos = Point::new(raw_player_pos.x, raw_player_pos.y);
         }
 
-        for (mut viewshed, _enemy, mut pos) in (&mut viewsheds, &enemies, &mut positions).join() {
+        for (mut viewshed, _enemy, mut pos, mut facing, mut renderable) in 
+            (&mut viewsheds, &enemies, &mut positions, &mut facings, &mut renderables).join() {
         
             let distance = rltk::DistanceAlg::Pythagoras.distance2d(Point::new(pos.x, pos.y), player_pos);
             if distance < 1.5 {
@@ -39,13 +42,64 @@ impl<'a> System<'a> for EnemyAI {
                     &mut *map
                 );
                 if path.success && path.steps.len() > 1 {
-                    let mut idx = map.xy_idx(pos.x, pos.y);
-                    map.blocked_tiles[idx] = false;
-                    pos.x = path.steps[1] as i32 % map.width;
-                    pos.y = path.steps[1] as i32 / map.width;
-                    idx = map.xy_idx(pos.x, pos.y);
-                    map.blocked_tiles[idx] = true;
-                    viewshed.dirty = true;
+                    let step_x = path.steps[1] as i32 % map.width;
+                    let step_y = path.steps[1] as i32 / map.width;
+
+                    let mut did_turn = false;
+
+                    let new_direction;
+                    if pos.x - step_x == 0 && pos.y - step_y == 1 { new_direction = Direction::UP }
+                    else if pos.x - step_x == -1 && pos.y - step_y == 1 { new_direction = Direction::UPRIGHT }
+                    else if pos.x - step_x == -1 && pos.y - step_y == 0 { new_direction = Direction::RIGHT }
+                    else if pos.x - step_x == -1 && pos.y - step_y == -1 { new_direction = Direction::DOWNRIGHT }
+                    else if pos.x - step_x == 0 && pos.y - step_y == -1 { new_direction = Direction::DOWN }
+                    else if pos.x - step_x == 1 && pos.y - step_y == -1 { new_direction = Direction::DOWNLEFT }
+                    else if pos.x - step_x == 1 && pos.y - step_y == 0 { new_direction = Direction::LEFT }
+                    else if pos.x - step_x == 1 && pos.y - step_y == 1 { new_direction = Direction::UPLEFT }
+                    else { new_direction = Direction::UP }
+
+                    if new_direction != facing.direction {
+                        facing.direction = new_direction;
+                        viewshed.dirty = true;
+                        did_turn = true;
+                    }
+                    match new_direction {
+                        Direction::UP => {
+                            renderable.glyph = rltk::to_cp437('8');
+                        },
+                        Direction::UPRIGHT => {
+                            renderable.glyph = rltk::to_cp437('9');
+                        },
+                        Direction::RIGHT => {
+                            renderable.glyph = rltk::to_cp437('6');
+                        },
+                        Direction::DOWNRIGHT => {
+                            renderable.glyph = rltk::to_cp437('3');
+                        },
+                        Direction::DOWN => {
+                            renderable.glyph = rltk::to_cp437('2');
+                        },
+                        Direction::DOWNLEFT => {
+                            renderable.glyph = rltk::to_cp437('1');
+                        },
+                        Direction::LEFT => {
+                            renderable.glyph = rltk::to_cp437('4');
+                        },
+                        Direction::UPLEFT => {
+                            renderable.glyph = rltk::to_cp437('7');
+                        }
+                    }
+                    if !did_turn {
+                        let mut idx = map.xy_idx(pos.x, pos.y);
+                        map.blocked_tiles[idx] = false;
+
+                        pos.x = step_x;
+                        pos.y = step_y;
+
+                        idx = map.xy_idx(pos.x, pos.y);
+                        map.blocked_tiles[idx] = true;
+                        viewshed.dirty = true;
+                    }
                 }
             }
         }
