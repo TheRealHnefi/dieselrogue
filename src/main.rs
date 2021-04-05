941fa233e037a25b69ad5b7a88f1e3eefce8295d
Johan Larsson <johan.t.larsson@gmail.com>
2021-04-05 10:12:15 +0200

    Basic state and hellow world output.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use rltk::{Rltk};
+use specs::prelude::*;
+
+mod state;
+use state::*;
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+
+    let context = RltkBuilder::simple80x50()
+        .with_title("Diesel Rogue")
+        .build()?;
+    
+    let mut game_state = State {
+        ecs: World::new()
+    };
+
+    rltk::main_loop(context, game_state)
 }
