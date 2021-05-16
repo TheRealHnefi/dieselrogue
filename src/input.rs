58ba9c2d628c6997e7784f7fd9ea92789fa16093
Johan Larsson <johan.t.larsson@gmail.com>
2021-05-16 09:20:18 +0200

    Make menus work.

--- src/input.rs
+++ src/input.rs
@@ -5,60 +5,60 @@ use std::cmp::{min, max};
 pub fn main_screen_input(state: &mut State, ctx: &mut Rltk) -> RunState {
     match ctx.key {
         Some(key) => match key {
-            VirtualKeyCode::Left |
-            VirtualKeyCode::Numpad4 |
-            VirtualKeyCode::H => try_move_player(Direction::Left, &mut state.ecs),
+            // VirtualKeyCode::Left |
+            // VirtualKeyCode::Numpad4 |
+            // VirtualKeyCode::H => try_move_player(Direction::Left, &mut state.ecs),
 
-            VirtualKeyCode::Right |
-            VirtualKeyCode::Numpad6 |
-            VirtualKeyCode::L => try_move_player(Direction::Right, &mut state.ecs),
+            // VirtualKeyCode::Right |
+            // VirtualKeyCode::Numpad6 |
+            // VirtualKeyCode::L => try_move_player(Direction::Right, &mut state.ecs),
 
-            VirtualKeyCode::Up |
-            VirtualKeyCode::Numpad8 |
-            VirtualKeyCode::K => try_move_player(Direction::Up, &mut state.ecs),
+            // VirtualKeyCode::Up |
+            // VirtualKeyCode::Numpad8 |
+            // VirtualKeyCode::K => try_move_player(Direction::Up, &mut state.ecs),
 
-            VirtualKeyCode::Down |
-            VirtualKeyCode::Numpad2 |
-            VirtualKeyCode::J => try_move_player(Direction::Down, &mut state.ecs),
+            // VirtualKeyCode::Down |
+            // VirtualKeyCode::Numpad2 |
+            // VirtualKeyCode::J => try_move_player(Direction::Down, &mut state.ecs),
 
-            VirtualKeyCode::Numpad9 |
-            VirtualKeyCode::Y => try_move_player(Direction::UpRight, &mut state.ecs),
+            // VirtualKeyCode::Numpad9 |
+            // VirtualKeyCode::Y => try_move_player(Direction::UpRight, &mut state.ecs),
 
-            VirtualKeyCode::Numpad7 |
-            VirtualKeyCode::U => try_move_player(Direction::UpLeft, &mut state.ecs),
+            // VirtualKeyCode::Numpad7 |
+            // VirtualKeyCode::U => try_move_player(Direction::UpLeft, &mut state.ecs),
 
-            VirtualKeyCode::Numpad3 |
-            VirtualKeyCode::N => try_move_player(Direction::DownRight, &mut state.ecs),
+            // VirtualKeyCode::Numpad3 |
+            // VirtualKeyCode::N => try_move_player(Direction::DownRight, &mut state.ecs),
 
-            VirtualKeyCode::Numpad1 |
-            VirtualKeyCode::B => try_move_player(Direction::DownLeft, &mut state.ecs),
+            // VirtualKeyCode::Numpad1 |
+            // VirtualKeyCode::B => try_move_player(Direction::DownLeft, &mut state.ecs),
 
-            VirtualKeyCode::Numpad5 => {},
+            // VirtualKeyCode::Numpad5 => {},
 
-            VirtualKeyCode::G => {
-                let mut game_log = state.ecs.fetch_mut::<GameLog>();
-                match get_item(&state.ecs) {
-                    Ok(_) => (),
-                    Err(_) => {
-                        game_log.entries.push("Can't pick up item".to_string());
-                        return RunState::AwaitingInput;
-                    }
-                }
-            },
+            // VirtualKeyCode::G => {
+            //     let mut game_log = state.ecs.fetch_mut::<GameLog>();
+            //     match get_item(&state.ecs) {
+            //         Ok(_) => (),
+            //         Err(_) => {
+            //             game_log.entries.push("Can't pick up item".to_string());
+            //             return RunState::AwaitingInput;
+            //         }
+            //     }
+            // },
 
-            VirtualKeyCode::I => {
-                return RunState::InventoryScreen;
-            },
+            // VirtualKeyCode::I => {
+            //     return RunState::InventoryScreen;
+            // },
 
-            VirtualKeyCode::T => {
-                let player = *state.ecs.fetch::<Entity>();
-                let positions = state.ecs.read_storage::<Position>();
-                let player_pos = positions.get(player).expect("Could not get player position");
-                let mut cursor_pos = state.ecs.fetch_mut::<Point>();
-                cursor_pos.x = player_pos.x;
-                cursor_pos.y = player_pos.y;
-                return RunState::TargetingInput;
-            },
+            // VirtualKeyCode::T => {
+            //     let player = *state.ecs.fetch::<Entity>();
+            //     let positions = state.ecs.read_storage::<Position>();
+            //     let player_pos = positions.get(player).expect("Could not get player position");
+            //     let mut cursor_pos = state.ecs.fetch_mut::<Point>();
+            //     cursor_pos.x = player_pos.x;
+            //     cursor_pos.y = player_pos.y;
+            //     return RunState::TargetingInput;
+            // },
 
             VirtualKeyCode::Escape => {
                 state.menu_stack.clear();
@@ -78,84 +78,85 @@ pub fn main_screen_input(state: &mut State, ctx: &mut Rltk) -> RunState {
 }
 
 pub fn targeting_input(state: &mut State, context: &mut Rltk) -> RunState {
-    match context.key {
-        Some(key) => match key {
-            VirtualKeyCode::Left |
-            VirtualKeyCode::Numpad4 => {
-                let mut cursor_pos = state.ecs.fetch_mut::<Point>();
-                cursor_pos.x = max(cursor_pos.x - 1, 0);
-            },
-            VirtualKeyCode::Right |
-            VirtualKeyCode::Numpad6 => {
-                let mut cursor_pos = state.ecs.fetch_mut::<Point>();
-                let map = state.ecs.fetch::<Map>();
-                cursor_pos.x = min(cursor_pos.x + 1, map.width - 1);
-            },
-            VirtualKeyCode::Up |
-            VirtualKeyCode::Numpad8 => {
-                let mut cursor_pos = state.ecs.fetch_mut::<Point>();
-                cursor_pos.y = max(cursor_pos.y - 1, 0);
-            },
-            VirtualKeyCode::Down |
-            VirtualKeyCode::Numpad2 => {
-                let mut cursor_pos = state.ecs.fetch_mut::<Point>();
-                let map = state.ecs.fetch::<Map>();
-                cursor_pos.y = min(cursor_pos.y + 1, map.height - 1);
-            },
-            VirtualKeyCode::Numpad9 => {
-                let mut cursor_pos = state.ecs.fetch_mut::<Point>();
-                cursor_pos.y = max(cursor_pos.y - 1, 0);
-                let map = state.ecs.fetch::<Map>();
-                cursor_pos.x = min(cursor_pos.x + 1, map.width - 1);
-            },
-            VirtualKeyCode::Numpad7 => {
-                let mut cursor_pos = state.ecs.fetch_mut::<Point>();
-                cursor_pos.x = max(cursor_pos.x - 1, 0);
-                cursor_pos.y = max(cursor_pos.y - 1, 0);
-            },
-            VirtualKeyCode::Numpad3 => {
-                let mut cursor_pos = state.ecs.fetch_mut::<Point>();
-                let map = state.ecs.fetch::<Map>();
-                cursor_pos.x = min(cursor_pos.x + 1, map.width - 1);
-                cursor_pos.y = min(cursor_pos.y + 1, map.height - 1);
-            },
-            VirtualKeyCode::Numpad1 => {
-                let mut cursor_pos = state.ecs.fetch_mut::<Point>();
-                let map = state.ecs.fetch::<Map>();
-                cursor_pos.y = min(cursor_pos.y + 1, map.height - 1);
-                cursor_pos.x = max(cursor_pos.x - 1, 0);
-            },
-            VirtualKeyCode::Escape => {
-                return RunState::AwaitingInput;
-            },
-            VirtualKeyCode::Space |
-            VirtualKeyCode::Return |
-            VirtualKeyCode::T => {
-                let cursor_pos = state.ecs.fetch::<Point>();
-                let map = state.ecs.fetch::<Map>();
-                let index = map.xy_idx(cursor_pos.x, cursor_pos.y);
-                let maybe_actor = map.tile_blockers[index];
-                // TODO: Iterate over all entities with this position and, in case of >1 hit, create menu
-                // to choose which to focus on
-                match maybe_actor {
-                    Some(entity) => {
-                        state.menu_stack.clear();
-                        state.menu_stack.push(Menu::new_target_menu(&state.ecs, cursor_pos.x, cursor_pos.y, entity));
-                        return RunState::MenuInput;
-                    }
-                    None => {
-                        return RunState::AwaitingInput;
-                    }
-                }
-            },
-            _ => {
-            }
-        }
-        None => {
-            return RunState::TargetingInput;
-        }
-    }
-    RunState::TargetingInput
+    // match context.key {
+    //     Some(key) => match key {
+    //         VirtualKeyCode::Left |
+    //         VirtualKeyCode::Numpad4 => {
+    //             let mut cursor_pos = state.ecs.fetch_mut::<Point>();
+    //             cursor_pos.x = max(cursor_pos.x - 1, 0);
+    //         },
+    //         VirtualKeyCode::Right |
+    //         VirtualKeyCode::Numpad6 => {
+    //             let mut cursor_pos = state.ecs.fetch_mut::<Point>();
+    //             let map = state.ecs.fetch::<Map>();
+    //             cursor_pos.x = min(cursor_pos.x + 1, map.width - 1);
+    //         },
+    //         VirtualKeyCode::Up |
+    //         VirtualKeyCode::Numpad8 => {
+    //             let mut cursor_pos = state.ecs.fetch_mut::<Point>();
+    //             cursor_pos.y = max(cursor_pos.y - 1, 0);
+    //         },
+    //         VirtualKeyCode::Down |
+    //         VirtualKeyCode::Numpad2 => {
+    //             let mut cursor_pos = state.ecs.fetch_mut::<Point>();
+    //             let map = state.ecs.fetch::<Map>();
+    //             cursor_pos.y = min(cursor_pos.y + 1, map.height - 1);
+    //         },
+    //         VirtualKeyCode::Numpad9 => {
+    //             let mut cursor_pos = state.ecs.fetch_mut::<Point>();
+    //             cursor_pos.y = max(cursor_pos.y - 1, 0);
+    //             let map = state.ecs.fetch::<Map>();
+    //             cursor_pos.x = min(cursor_pos.x + 1, map.width - 1);
+    //         },
+    //         VirtualKeyCode::Numpad7 => {
+    //             let mut cursor_pos = state.ecs.fetch_mut::<Point>();
+    //             cursor_pos.x = max(cursor_pos.x - 1, 0);
+    //             cursor_pos.y = max(cursor_pos.y - 1, 0);
+    //         },
+    //         VirtualKeyCode::Numpad3 => {
+    //             let mut cursor_pos = state.ecs.fetch_mut::<Point>();
+    //             let map = state.ecs.fetch::<Map>();
+    //             cursor_pos.x = min(cursor_pos.x + 1, map.width - 1);
+    //             cursor_pos.y = min(cursor_pos.y + 1, map.height - 1);
+    //         },
+    //         VirtualKeyCode::Numpad1 => {
+    //             let mut cursor_pos = state.ecs.fetch_mut::<Point>();
+    //             let map = state.ecs.fetch::<Map>();
+    //             cursor_pos.y = min(cursor_pos.y + 1, map.height - 1);
+    //             cursor_pos.x = max(cursor_pos.x - 1, 0);
+    //         },
+    //         VirtualKeyCode::Escape => {
+    //             return RunState::AwaitingInput;
+    //         },
+    //         VirtualKeyCode::Space |
+    //         VirtualKeyCode::Return |
+    //         VirtualKeyCode::T => {
+    //             let cursor_pos = state.ecs.fetch::<Point>();
+    //             let map = state.ecs.fetch::<Map>();
+    //             let index = map.xy_idx(cursor_pos.x, cursor_pos.y);
+    //             let maybe_actor = map.tile_blockers[index];
+    //             // TODO: Iterate over all entities with this position and, in case of >1 hit, create menu
+    //             // to choose which to focus on
+    //             match maybe_actor {
+    //                 Some(entity) => {
+    //                     state.menu_stack.clear();
+    //                     state.menu_stack.push(Menu::new_target_menu(&state.ecs, cursor_pos.x, cursor_pos.y, entity));
+    //                     return RunState::MenuInput;
+    //                 }
+    //                 None => {
+    //                     return RunState::AwaitingInput;
+    //                 }
+    //             }
+    //         },
+    //         _ => {
+    //         }
+    //     }
+    //     None => {
+    //         return RunState::TargetingInput;
+    //     }
+    // }
+    // RunState::TargetingInput
+    RunState::AwaitingInput
 }
 
 pub fn menu_input(state: &mut State, ctx: &mut Rltk) -> RunState {
@@ -209,55 +210,56 @@ pub fn menu_input(state: &mut State, ctx: &mut Rltk) -> RunState {
 }
 
 pub fn inventory_screen_input(state: &mut State, ctx: &mut Rltk) -> RunState {
-    match ctx.key {
-        Some(key) => {
-            let player = state.ecs.fetch::<Entity>();
-            let inventories = state.ecs.read_storage::<Inventory>();
-            let inventory = inventories.get(*player).unwrap();
-            let mut game_log = state.ecs.fetch_mut::<GameLog>();
+    // match ctx.key {
+    //     Some(key) => {
+    //         let player = state.ecs.fetch::<Entity>();
+    //         let inventories = state.ecs.read_storage::<Inventory>();
+    //         let inventory = inventories.get(*player).unwrap();
+    //         let mut game_log = state.ecs.fetch_mut::<GameLog>();
             
-            match key {
-                VirtualKeyCode::Escape |
-                VirtualKeyCode::I => {
-                    return RunState::AwaitingInput;
-                },
-                VirtualKeyCode::Down |
-                VirtualKeyCode::Numpad2 => {
-                    state.inventory_screen_selection = min(state.inventory_screen_selection + 1,
-                                                                inventory.items.len() as i32 - 1);           
-                    return RunState::InventoryScreen;
-                },
-                VirtualKeyCode::Up |
-                VirtualKeyCode::Numpad8 => {
-                    state.inventory_screen_selection = max(state.inventory_screen_selection - 1, 0);
-                    return RunState::InventoryScreen;
-                },
-                VirtualKeyCode::D => {
-                    match drop_item(&state.ecs, inventory.items[state.inventory_screen_selection as usize]) {
-                        Ok(_) => return RunState::PlayerTurn,
-                        Err(_) => {
-                            game_log.entries.push("Can't drop item. Is something in the way?".to_string());
-                            return RunState::AwaitingInput;
-                        }
-                    }
-                },
-                VirtualKeyCode::Space |
-                VirtualKeyCode::Return => {
-                    match equip_item(&state.ecs, inventory.items[state.inventory_screen_selection as usize]) {
-                        Ok(_) => return RunState::PlayerTurn,
-                        Err(_) => {
-                            game_log.entries.push("Can't equip item".to_string());
-                            return RunState::AwaitingInput;
-                        }
-                    }
-                },
-                _ => {
-                    return RunState::InventoryScreen;
-                }
-            }
-        }
-        None => {
-            return RunState::InventoryScreen;
-        }
-    }
+    //         match key {
+    //             VirtualKeyCode::Escape |
+    //             VirtualKeyCode::I => {
+    //                 return RunState::AwaitingInput;
+    //             },
+    //             VirtualKeyCode::Down |
+    //             VirtualKeyCode::Numpad2 => {
+    //                 state.inventory_screen_selection = min(state.inventory_screen_selection + 1,
+    //                                                             inventory.items.len() as i32 - 1);           
+    //                 return RunState::InventoryScreen;
+    //             },
+    //             VirtualKeyCode::Up |
+    //             VirtualKeyCode::Numpad8 => {
+    //                 state.inventory_screen_selection = max(state.inventory_screen_selection - 1, 0);
+    //                 return RunState::InventoryScreen;
+    //             },
+    //             VirtualKeyCode::D => {
+    //                 match drop_item(&state.ecs, inventory.items[state.inventory_screen_selection as usize]) {
+    //                     Ok(_) => return RunState::PlayerTurn,
+    //                     Err(_) => {
+    //                         game_log.entries.push("Can't drop item. Is something in the way?".to_string());
+    //                         return RunState::AwaitingInput;
+    //                     }
+    //                 }
+    //             },
+    //             VirtualKeyCode::Space |
+    //             VirtualKeyCode::Return => {
+    //                 match equip_item(&state.ecs, inventory.items[state.inventory_screen_selection as usize]) {
+    //                     Ok(_) => return RunState::PlayerTurn,
+    //                     Err(_) => {
+    //                         game_log.entries.push("Can't equip item".to_string());
+    //                         return RunState::AwaitingInput;
+    //                     }
+    //                 }
+    //             },
+    //             _ => {
+    //                 return RunState::InventoryScreen;
+    //             }
+    //         }
+    //     }
+    //     None => {
+    //         return RunState::InventoryScreen;
+    //     }
+    // }
+    RunState::AwaitingInput
 }
--- src/state.rs
+++ src/state.rs
@@ -20,7 +20,7 @@ pub struct State {
     pub mouse_pos: Point,
     pub menu_stack: Vec<Menu>,
     pub inventory_screen_selection: i32,
-
+    pub run_state: RunState,
     last_tick: Instant,
 }
 
@@ -31,7 +31,8 @@ impl State {
             last_tick: Instant::now(),
             mouse_pos: Point {x: 0, y:0},
             menu_stack: Vec::new(),
-            inventory_screen_selection: 0
+            inventory_screen_selection: 0,
+            run_state: RunState::PreRun
         }
     }
 
@@ -45,76 +46,70 @@ impl GameState for State {
         
         context.cls();
 
-        // let mut new_run_state;
-        // {
-        //     let run_state = self.ecs.fetch::<RunState>();
-        //     new_run_state = *run_state;
-        // }
-        // match new_run_state {
-        //     RunState::PreRun => {
-        //         self.run_systems();
-        //         new_run_state = RunState::AwaitingInput;
-        //         draw_main_screen(self, context);
-        //     },
-        //     RunState::AwaitingInput => {
-        //         new_run_state = main_screen_input(self, context);
-        //         draw_main_screen(self, context);
-        //     },
-        //     RunState::MenuInput => {
-        //         new_run_state = menu_input(self, context);
-        //         draw_main_screen(self, context);
-        //     },
-        //     RunState::TargetingInput => {
-        //         new_run_state = targeting_input(self, context);
-        //         draw_main_screen(self, context);
-        //     }
-        //     RunState::PlayerTurn => {
-        //         self.run_systems();
-        //         new_run_state = RunState::EnemyTurn;
-        //         draw_main_screen(self, context);
-        //     },
-        //     RunState::EnemyTurn => {
-        //         self.run_systems();
-        //         new_run_state = RunState::AwaitingInput;
-        //         draw_main_screen(self, context);
-        //     },
-        //     RunState::Saving => {
-        //         {
-        //             let result = saveload_system::save_game(&mut self.ecs);
-        //             let mut game_log = self.ecs.fetch_mut::<GameLog>();
-        //             match result {
-        //                 Ok(_) => game_log.entries.push("Game saved.".to_string()),
-        //                 Err(_) => game_log.entries.push("Game could not be saved.".to_string())
-        //             }
-        //             new_run_state = RunState::AwaitingInput;
-        //         }
-        //         draw_main_screen(self, context);
-        //     },
-        //     RunState::Loading => {
-        //         {
-        //             let result = saveload_system::load_game(&mut self.ecs);
-        //             let mut game_log = self.ecs.fetch_mut::<GameLog>();
-        //             match result {
-        //                 Ok(_) => game_log.entries.push("Game loaded.".to_string()),
-        //                 Err(_) => game_log.entries.push("Game could not be loaded.".to_string())
-        //             }
-        //             new_run_state = RunState::AwaitingInput;
-        //         }
-        //         draw_main_screen(self, context);
-        //     },
-        //     RunState::InventoryScreen => {
-        //         new_run_state = inventory_screen_input(self, context);
-        //         draw_inventory_screen(self, context);
-        //     }
-        // }
-        // {
-        //     let mut run_writer = self.ecs.write_resource::<RunState>();
-        //     *run_writer = new_run_state;
-        // }
+        match self.run_state {
+            RunState::PreRun => {
+                self.run_systems();
+                self.run_state = RunState::AwaitingInput;
+                draw_main_screen(self, context);
+            },
+            RunState::AwaitingInput => {
+                self.run_state = main_screen_input(self, context);
+                draw_main_screen(self, context);
+            },
+            RunState::MenuInput => {
+                self.run_state = menu_input(self, context);
+                draw_main_screen(self, context);
+            },
+            RunState::TargetingInput => {
+                self.run_state = targeting_input(self, context);
+                draw_main_screen(self, context);
+            }
+            RunState::PlayerTurn => {
+                self.run_systems();
+                self.run_state = RunState::EnemyTurn;
+                draw_main_screen(self, context);
+            },
+            RunState::EnemyTurn => {
+                self.run_systems();
+                self.run_state = RunState::AwaitingInput;
+                draw_main_screen(self, context);
+            },
+            RunState::Saving => {
+                // {
+                //     let result = saveload_system::save_game(&mut self.ecs);
+                //     let mut game_log = self.ecs.fetch_mut::<GameLog>();
+                //     match result {
+                //         Ok(_) => game_log.entries.push("Game saved.".to_string()),
+                //         Err(_) => game_log.entries.push("Game could not be saved.".to_string())
+                //     }
+                //     new_run_state = RunState::AwaitingInput;
+                // }
+                // draw_main_screen(self, context);
+                self.run_state = RunState::AwaitingInput;
+            },
+            RunState::Loading => {
+                // {
+                //     let result = saveload_system::load_game(&mut self.ecs);
+                //     let mut game_log = self.ecs.fetch_mut::<GameLog>();
+                //     match result {
+                //         Ok(_) => game_log.entries.push("Game loaded.".to_string()),
+                //         Err(_) => game_log.entries.push("Game could not be loaded.".to_string())
+                //     }
+                //     new_run_state = RunState::AwaitingInput;
+                // }
+                // draw_main_screen(self, context);
+                self.run_state = RunState::AwaitingInput;
+            },
+            RunState::InventoryScreen => {
+                // new_run_state = inventory_screen_input(self, context);
+                // draw_inventory_screen(self, context);
+                self.run_state = RunState::AwaitingInput;
+            }
+        }
 
-        // if new_run_state == RunState::MenuInput {
-        //     draw_menu(self, context);
-        // }
+        if self.run_state == RunState::MenuInput {
+            draw_menu(self, context);
+        }
 
         let tick_time = begin.elapsed().as_micros();
         if tick_time > 6000 {
