ac1b82ba334dd0983635f8c6b0782f54b5c0c13e
Johan Larsson <johan.t.larsson@gmail.com>
2022-01-09 11:40:15 +0100

    Add a bunch of items.

--- doc/backlog.md
+++ doc/backlog.md
@@ -1,11 +1,15 @@
+* Most items
+* Refactor item abilities and innate abilities
+ * Make abilities simple enums
+* Add a few non-item abilities
 * UI redesign
- * Better main UI
  * Minimap
  * Better inventory screen
  * Better ability menus
  * Show outcome of actions
  * Show more information about enemies and world
  * Optimize map drawing (RLTK batched rendering does not seem to help much. Other approaches?)
+ * Use different fonts for different resolutions to fix scaling issues
 * Noise (remember, only the noisiest noise is heard by most characters)
 * Map building
  * Prisons
@@ -27,7 +31,6 @@
  * Openable
 * Animations
  * Screen culling
-* Add a few non-item abilities
 * Aiming
  * Playtest - should I really use this aiming system?
  * Make entity able to aim at other entities (not just positions)
@@ -40,13 +43,11 @@
  * Fire only in FoV
 * Limit inventory size to 20
 * Refactor player functions
-* Refactor item abilities and innate abilities
 * Refactor menu system (oh my god it sucks!)
 * Refactor action resolution
 * Death effects (drop items, corpses etc, game over screen)
 * Fluids (maybe)
 * Most abilities
-* Most items
 * Create AI's
  * Create new pathfinding system
  * Put AI's on worker thread
--- src/item.rs
+++ src/item.rs
@@ -17,28 +17,26 @@ pub struct Item {
 }
 
 impl Item {
-    pub fn grenade() -> Self {
+    pub fn revolver() -> Self {
         Item {
             id: 0,
-            renderable: Renderable::new_char('g'),
-            name: String::from("Grenade"),
-            inventory_actions: vec![Item::throw_action(), Item::drop_action()],
-            equip_actions: vec!(),
-            equip_slots: vec!(),
-            kind: ItemKind::Misc,
-            proxy: false
-        }
-    }
-
-    pub fn rocket_launcher() -> Self {
-        Item {
-            id: 0,
-            renderable: Renderable::new_char('r'),
-            name: String::from("Rocket launcher"),
-            inventory_actions: vec![Item::equip_action(), Item::drop_action()],
-            equip_actions: vec!(Item::aim_action(), Item::fire_action()),
-            equip_slots: vec!(SlotType::PrimaryHand, SlotType::SecondaryHand),
-            kind: ItemKind::Firearm {ammo: 1, max_ammo: 1, damage: Damage::new(500, 0, 0)},
+            renderable: Renderable::new_char('p'),
+            name: String::from("Revolver"),
+            inventory_actions: vec!(
+                Item::equip_action(),
+                Item::drop_action()
+            ),
+            equip_actions: vec!(
+                Item::aim_action(),
+                Item::fire_action()
+            ),
+            equip_slots: vec!(SlotType::PrimaryHand),
+            kind: ItemKind::Firearm {
+                ammo: 6,
+                max_ammo: 6,
+                damage: Damage::new(15, 0, 0, 0),
+                range: 5
+            },
             proxy: false
         }
     }
@@ -48,10 +46,127 @@ impl Item {
             id: 0,
             renderable: Renderable::new_char('p'),
             name: String::from("Pistol"),
-            inventory_actions: vec![Item::equip_action(), Item::drop_action()],
-            equip_actions: vec!(Item::aim_action(), Item::fire_action()),
+            inventory_actions: vec!(
+                Item::equip_action(), Item::drop_action()
+            ),
+            equip_actions: vec!(
+                Item::aim_action(),
+                Item::fire_action()
+            ),
             equip_slots: vec!(SlotType::PrimaryHand),
-            kind: ItemKind::Firearm {ammo: 5, max_ammo: 5, damage: Damage::new(5, 0, 0)},
+            kind: ItemKind::Firearm {
+                ammo: 12,
+                max_ammo: 12,
+                damage: Damage::new(10, 0, 0, 0),
+                range: 5
+            },
+            proxy: false
+        }
+    }
+
+    pub fn submachine_gun() -> Self {
+        Item {
+            id: 0,
+            renderable: Renderable::new_char('p'),
+            name: String::from("SMG"),
+            inventory_actions: vec!(
+                Item::equip_action(),
+                Item::drop_action()
+            ),
+            equip_actions: vec!(
+                Item::aim_action(),
+                Item::fire_action(),
+                Item::fire_burst_action()
+            ),
+            equip_slots: vec!(SlotType::PrimaryHand),
+            kind: ItemKind::Firearm {
+                ammo: 25,
+                max_ammo: 25,
+                damage: Damage::new(10, 0, 0, 0),
+                range: 5
+            },
+            proxy: false
+        }
+    }
+
+    pub fn bolt_action_rifle() -> Self {
+        Item {
+            id: 0,
+            renderable: Renderable::new_char('p'),
+            name: String::from("Bolt action rifle"),
+            inventory_actions: vec!(
+                Item::equip_action(),
+                Item::drop_action()
+            ),
+            equip_actions: vec!(
+                Item::aim_action(),
+                Item::fire_action()
+            ),
+            equip_slots: vec!(
+                SlotType::PrimaryHand,
+                SlotType::SecondaryHand
+            ),
+            kind: ItemKind::Firearm {
+                ammo: 5,
+                max_ammo: 5,
+                damage: Damage::new(25, 0, 0, 0),
+                range: 15
+            },
+            proxy: false
+        }
+    }
+
+    pub fn semi_auto_rifle() -> Self {
+        Item {
+            id: 0,
+            renderable: Renderable::new_char('p'),
+            name: String::from("Semi-automatic rifle"),
+            inventory_actions: vec!(
+                Item::equip_action(),
+                Item::drop_action()
+            ),
+            equip_actions: vec!(
+                Item::aim_action(),
+                Item::fire_action()
+            ),
+            equip_slots: vec!(
+                SlotType::PrimaryHand,
+                SlotType::SecondaryHand
+            ),
+            kind: ItemKind::Firearm {
+                ammo: 10,
+                max_ammo: 10,
+                damage: Damage::new(20, 0, 0, 0),
+                range: 15
+            },
+            proxy: false
+        }
+    }
+
+    pub fn assault_rifle() -> Self {
+        Item {
+            id: 0,
+            renderable: Renderable::new_char('m'),
+            name: String::from("Assault rifle"),
+            inventory_actions: vec!(
+                Item::equip_action(),
+                Item::drop_action()
+            ),
+            equip_actions: vec!(
+                Item::aim_action(),
+                Item::fire_action(),
+                Item::fire_burst_action()
+            ),
+            equip_slots: vec!(
+                SlotType::PrimaryHand,
+                SlotType::SecondaryHand
+            ),
+            kind: ItemKind::Firearm {
+                ammo: 25,
+                max_ammo: 25,
+                damage: Damage::new(15, 0, 0, 0),
+                range: 12
+            },
             proxy: false
         }
     }
@@ -60,14 +175,249 @@ impl Item {
         Item {
             id: 0,
             renderable: Renderable::new_char('m'),
-            name: String::from("Machinegun"),
-            inventory_actions: vec![Item::equip_action(), Item::drop_action()],
-            equip_actions: vec!(Item::aim_action(), Item::fire_action(), Item::fire_burst_action()),
-            equip_slots: vec!(SlotType::PrimaryHand, SlotType::SecondaryHand),
-            kind: ItemKind::Firearm {ammo: 30, max_ammo: 30, damage: Damage::new(5, 0, 0)},
+            name: String::from("Machine gun"),
+            inventory_actions: vec!(
+                Item::equip_action(),
+                Item::drop_action()
+            ),
+            equip_actions: vec!(
+                Item::aim_action(),
+                Item::fire_burst_action()
+            ),
+            equip_slots: vec!(
+                SlotType::PrimaryHand,
+                SlotType::SecondaryHand
+            ),
+            kind: ItemKind::Firearm {
+                ammo: 30,
+                max_ammo: 30,
+                damage: Damage::new(15, 0, 0, 0),
+                range: 10
+            },
             proxy: false
         }
     }
+
+    pub fn rotary_machinegun() -> Self {
+        Item {
+            id: 0,
+            renderable: Renderable::new_char('m'),
+            name: String::from("Rotary machine gun"),
+            inventory_actions: vec!(
+                Item::equip_action(),
+                Item::drop_action()
+            ),
+            equip_actions: vec!(
+                Item::aim_action(),
+                Item::fire_burst_action()
+            ),
+            equip_slots: vec!(
+                SlotType::PrimaryHand,
+                SlotType::SecondaryHand
+            ),
+            kind: ItemKind::Firearm {
+                ammo: 100,
+                max_ammo: 100,
+                damage: Damage::new(12, 0, 0, 0),
+                range: 10
+            },
+            proxy: false
+        }
+    }
+
+    pub fn rocket_launcher() -> Self {
+        Item {
+            id: 0,
+            renderable: Renderable::new_char('r'),
+            name: String::from("Rocket launcher"),
+            inventory_actions: vec!(
+                Item::equip_action(),
+                Item::drop_action()
+            ),
+            equip_actions: vec!(
+                Item::aim_action(),
+                Item::fire_rocket_action()
+            ),
+            equip_slots: vec!(
+                SlotType::PrimaryHand,
+                SlotType::SecondaryHand
+            ),
+            kind: ItemKind::Firearm {
+                ammo: 1,
+                max_ammo: 1,
+                damage: Damage::new(500, 0, 0, 0),
+                range: 15
+            },
+            proxy: false
+        }
+    }
+
+    pub fn multi_rocket_launcher() -> Self {
+        Item {
+            id: 0,
+            renderable: Renderable::new_char('r'),
+            name: String::from("Multi-rocket launcher"),
+            inventory_actions: vec!(
+                Item::equip_action(),
+                Item::drop_action()
+            ),
+            equip_actions: vec!(
+                Item::aim_action(),
+                Item::fire_rocket_action()
+            ),
+            equip_slots: vec!(
+                SlotType::PrimaryHand,
+                SlotType::SecondaryHand
+            ),
+            kind: ItemKind::Firearm {
+                ammo: 4,
+                max_ammo: 4,
+                damage: Damage::new(100, 0, 0, 0),
+                range: 12
+            },
+            proxy: false
+        }
+    }
+
+    pub fn flamethrower() -> Self {
+        Item {
+            id: 0,
+            renderable: Renderable::new_char('r'),
+            name: String::from("Flamethrower"),
+            inventory_actions: vec!(
+                Item::equip_action(),
+                Item::drop_action()
+            ),
+            equip_actions: vec!(
+                Item::aim_action(),
+                Item::fan_fire_action()
+            ),
+            equip_slots: vec!(
+                SlotType::PrimaryHand,
+                SlotType::SecondaryHand
+            ),
+            kind: ItemKind::Firearm {
+                ammo: 10,
+                max_ammo: 10,
+                damage: Damage::new(0, 0, 10, 0),
+                range: 5
+            },
+            proxy: false
+        }
+    }
+
+    pub fn flare_gun() -> Self {
+        Item {
+            id: 0,
+            renderable: Renderable::new_char('r'),
+            name: String::from("Flare gun"),
+            inventory_actions: vec!(
+                Item::equip_action(),
+                Item::drop_action()
+            ),
+            equip_actions: vec!(
+                Item::aim_action(),
+                Item::fire_action()
+            ),
+            equip_slots: vec!(
+                SlotType::PrimaryHand
+            ),
+            kind: ItemKind::Firearm {
+                ammo: 1,
+                max_ammo: 1,
+                damage: Damage::new(0, 0, 10, 0),
+                range: 5
+            },
+            proxy: false
+        }
+    }
+
+    pub fn shock_pistol() -> Self {
+        Item {
+            id: 0,
+            renderable: Renderable::new_char('r'),
+            name: String::from("Shock pistol"),
+            inventory_actions: vec!(
+                Item::equip_action(),
+                Item::drop_action()
+            ),
+            equip_actions: vec!(
+                Item::aim_action(),
+                Item::fan_fire_action()
+            ),
+            equip_slots: vec!(
+                SlotType::PrimaryHand
+            ),
+            kind: ItemKind::Firearm {
+                ammo: 5,
+                max_ammo: 5,
+                damage: Damage::new(0, 5, 0, 0),
+                range: 3
+            },
+            proxy: false
+        }
+    }
+
+    pub fn shock_carbine() -> Self {
+        Item {
+            id: 0,
+            renderable: Renderable::new_char('r'),
+            name: String::from("Shock carbine"),
+            inventory_actions: vec!(
+                Item::equip_action(),
+                Item::drop_action()
+            ),
+            equip_actions: vec!(
+                Item::aim_action(),
+                Item::fan_fire_action()
+            ),
+            equip_slots: vec!(
+                SlotType::PrimaryHand,
+                SlotType::SecondaryHand
+            ),
+            kind: ItemKind::Firearm {
+                ammo: 15,
+                max_ammo: 15,
+                damage: Damage::new(0, 10, 0, 0),
+                range: 6
+            },
+            proxy: false
+        }
+    }
+
+    pub fn shock_cannon() -> Self {
+        Item {
+            id: 0,
+            renderable: Renderable::new_char('r'),
+            name: String::from("Shock cannon"),
+            inventory_actions: vec!(
+                Item::equip_action(),
+                Item::drop_action()
+            ),
+            equip_actions: vec!(
+                Item::aim_action(),
+                Item::fan_fire_action()
+            ),
+            equip_slots: vec!(
+                SlotType::PrimaryHand,
+                SlotType::SecondaryHand
+            ),
+            kind: ItemKind::Firearm {
+                ammo: 2,
+                max_ammo: 2,
+                damage: Damage::new(0, 25, 0, 0),
+                range: 8
+            },
+            proxy: false
+        }
+    }
+
+    // TODO:
+    // grenade_launcher
+    // frag_grenade
+    // shock_grenade
+    // fire_grenade
+    // flashbang
 
     pub fn bulletproof_vest() -> Self {
         Item {
@@ -140,6 +490,24 @@ impl Item {
             phase: IntentPhase::Attack,
             precondition: precondition_is_aiming,
             effects: Entity::resolve_burst_fire
+        }
+    }
+    fn fire_rocket_action() -> IntentAction {
+        IntentAction {
+            name: "Fire rocket".to_string(),
+            targeting: Targeting::Positional,
+            phase: IntentPhase::Attack,
+            precondition: precondition_is_aiming,
+            effects: Entity::resolve_rocket_fire
+        }
+    }
+    fn fan_fire_action() -> IntentAction {
+        IntentAction {
+            name: "Fan fire".to_string(),
+            targeting: Targeting::Positional,
+            phase: IntentPhase::Attack,
+            precondition: precondition_ok,
+            effects: Entity::resolve_fan_fire
         }
     }
     fn throw_action() -> IntentAction {
