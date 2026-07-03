bfa72ebe1d7fa92265bf739baa284717c977913d
Johan Larsson <johan.t.larsson@gmail.com>
2026-07-03 09:06:20 +0200

    Undo broken tank fix

--- src/sprite.rs
+++ src/sprite.rs
@@ -79,7 +79,7 @@ impl Sprite {
                     rltk::to_cp437('█'),
                     rltk::to_cp437('─'),
                     rltk::to_cp437('╙'),
-                    rltk::to_cp437('╘'),
+                    rltk::to_cp437('╨'),
                     rltk::to_cp437('╜'),
                 ]
             },
@@ -131,7 +131,7 @@ impl Sprite {
                     rltk::to_cp437('█'),
                     rltk::to_cp437('║'),
                     rltk::to_cp437('╙'),
-                    rltk::to_cp437('╘'),
+                    rltk::to_cp437('╨'),
                     rltk::to_cp437('╜'),
                 ]
             },
