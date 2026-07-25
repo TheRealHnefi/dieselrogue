3436ffef1d4df15f991ee48f2752c0417f40761a
Johan Larsson <hnefi.mail@gmail.com>
2026-07-25 11:43:16 +0200

    Add system for resolving melee attacks.

--- src/entity.rs
+++ src/entity.rs
@@ -2,9 +2,14 @@ use crate::components::*;
 use rltk::Point;
 use crate::Map;
 
+pub enum Effect {
+    Damage(usize) // damage(entity_id)
+}
+
 /// Concrete type containing all data of something that acts and moves.
 #[derive(Clone)]
 pub struct Entity {
+    pub id: usize,
     pub position: Point,
     pub renderable: Renderable,
     pub name: String,
@@ -15,6 +20,7 @@ pub struct Entity {
 impl Entity {
     pub fn create_pawn(&self) -> Pawn {
         Pawn {
+            entity_id: self.id,
             renderable: self.renderable,
             name: self.name.clone(),
             intent: self.intent,
@@ -22,17 +28,18 @@ impl Entity {
         }
     }
 
-    pub fn resolve(&mut self, map: &mut Map) {
+    pub fn resolve_movement(&mut self, map: &mut Map) -> Option<Effect> {
         let old_index = map.xy_idx(self.position.x, self.position.y);
         let mut new_index = old_index;
 
         match self.intent.action {
-            Action::Idle => {},
             Action::Move(pos) => {
                 if !map.blocked(pos.x, pos.y) {
                     new_index = map.xy_idx(pos.x, pos.y);
                     self.position = pos;
                 }
+
+                self.intent = Intent {action: Action::Idle};
             },
             Action::Turn(direction) => {
                 self.facing.direction = direction;
@@ -47,12 +54,30 @@ impl Entity {
                     Direction::Left => {self.renderable.glyph = rltk::to_cp437('4')},
                     Direction::UpLeft => {self.renderable.glyph = rltk::to_cp437('7')},
                 }
-            }
+
+                self.intent = Intent {action: Action::Idle};
+            },
+            _ => {}
         }
 
         map.pawns[old_index] = None;
         map.pawns[new_index] = Some(self.create_pawn());
-        self.intent = Intent {action: Action::Idle};
+        
+        None
+    }
+
+    pub fn resolve_melee(&mut self, map: &mut Map) -> Option<Effect> {
+        match self.intent.action {
+            Action::Melee(pos) => {
+                self.intent = Intent {action: Action::Idle};
+
+                let index = map.xy_idx(pos.x, pos.y);
+                // TODO: check existence
+                let id = map.pawns[index].as_ref().unwrap().entity_id;
+                Some(Effect::Damage(id))
+            },
+            _ => None
+        }
     }
 }
 
@@ -60,6 +85,7 @@ impl Entity {
 /// indexing.
 #[derive(Clone)]
 pub struct Pawn {
+    pub entity_id: usize,
     pub renderable: Renderable,
     pub name: String,
     pub intent: Intent,
