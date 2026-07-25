7c5be9110deffa4cabdeab185e7ee3ebd55f4128
Johan Larsson <hnefi.mail@gmail.com>
2026-07-25 11:43:02 +0200

    Completely gut all functionality to start purgin the ecs.

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,44 @@
-use specs::prelude::*;
-use specs::saveload::{MarkedBuilder, SimpleMarker, SimpleMarkerAllocator};
 use rltk::{Point};
 
 mod state;
 pub use state::*;
-mod components;
-pub use components::*;
-mod player;
-pub use player::*;
-mod map;
-pub use map::*;
-mod rect;
-pub use rect::*;
-mod visibility_system;
-pub use visibility_system::*;
-mod map_indexing_system;
-pub use map_indexing_system::*;
-mod enemy_ai_system;
-pub use enemy_ai_system::*;
-mod tank_ai_system;
-pub use tank_ai_system::*;
-mod damage_system;
-pub use damage_system::*;
+// mod components;
+// pub use components::*;
+// mod player;
+// pub use player::*;
+// mod map;
+// pub use map::*;
+// mod rect;
+// pub use rect::*;
+// mod visibility_system;
+// pub use visibility_system::*;
+// mod map_indexing_system;
+// pub use map_indexing_system::*;
+// mod enemy_ai_system;
+// pub use enemy_ai_system::*;
+// mod tank_ai_system;
+// pub use tank_ai_system::*;
+// mod damage_system;
+// pub use damage_system::*;
 mod ui;
 pub use ui::*;
 mod game_log;
 pub use game_log::*;
-mod inventory_system;
-pub use inventory_system::*;
-mod menu;
-pub use menu::*;
-mod input;
-pub use input::*;
-mod saveload_system;
-pub use saveload_system::*;
-mod serde_collections;
-pub use serde_collections::*;
-mod rex_assets;
-pub use rex_assets::*;
+// mod inventory_system;
+// pub use inventory_system::*;
+// mod menu;
+// pub use menu::*;
+// mod input;
+// pub use input::*;
+// mod saveload_system;
+// pub use saveload_system::*;
+// mod serde_collections;
+// pub use serde_collections::*;
+// mod rex_assets;
+// pub use rex_assets::*;
 
 #[derive(Debug)]
 pub struct GameError {
-}
-
-impl From<specs::error::Error> for GameError {
-    fn from(_err: specs::error::Error) -> Self {
-        GameError {}
-    }
 }
 
 impl From<()> for GameError {
@@ -71,211 +63,34 @@ fn main() -> rltk::BError {
 
     let mut game_state = State::new();
 
-    game_state.ecs.register::<SimpleMarker<SerializeMarker>>();
-    game_state.ecs.register::<SerializationHelper>();
-    game_state.ecs.register::<Player>();
-    game_state.ecs.register::<Enemy>();
-    game_state.ecs.register::<Position>();
-    game_state.ecs.register::<Size>();
-    game_state.ecs.register::<Direction>();
-    game_state.ecs.register::<ItemSlot>();
-    game_state.ecs.register::<Facing>();
-    game_state.ecs.register::<Vehicle>();
-    game_state.ecs.register::<Renderable>();
-    game_state.ecs.register::<LargeRenderable>();
-    game_state.ecs.register::<Viewshed>();
-    game_state.ecs.register::<Name>();
-    game_state.ecs.register::<BlocksTile>();
-    game_state.ecs.register::<Firearm>();
-    game_state.ecs.register::<Equippable>();
-    game_state.ecs.register::<Gettable>();
-    game_state.ecs.register::<GettingItem>();
-    game_state.ecs.register::<DroppingItem>();
-    game_state.ecs.register::<EquippingItem>();
-    game_state.ecs.register::<Inventory>();
-    game_state.ecs.register::<Protection>();
-    game_state.ecs.register::<Damage>();
-    game_state.ecs.register::<HumanoidBody>();
-
-    game_state.ecs.insert(SimpleMarkerAllocator::<SerializeMarker>::new());
-
-    let map = Map::new_map_rooms_and_corridors();
-    let (player_x, player_y) = map.rooms[0].center();
-
-    let player_entity = game_state.ecs
-        .create_entity()
-        .with(Position {
-            x: player_x,
-            y: player_y
-        })
-        .with(Renderable {
-            glyph: rltk::to_cp437('8'),
-            color: rltk::RGB::named(rltk::YELLOW),
-            background: rltk::RGB::named(rltk::BLACK)
-        })
-        .with(Player {})
-        .with(Viewshed {
-            visible_tiles: Vec::new(),
-            range: 10,
-            dirty: true
-        })
-        .with(Facing {direction: Direction::Up})
-        .with(HumanoidBody::new(20))
-        .with(Inventory {items: EntityVec::new()})
-        .with(Name {value: "Player".to_string()})
-        .marked::<SimpleMarker<SerializeMarker>>()
-        .build();
-    game_state.ecs.insert(player_entity);
-
-    let (enemy_x, enemy_y) = map.rooms[1].center();
-    game_state.ecs
-        .create_entity()
-        .with(Position {
-            x: enemy_x,
-            y: enemy_y
-        })
-        .with(Renderable {
-            glyph: rltk::to_cp437('8'),
-            color: rltk::RGB::named(rltk::RED),
-            background: rltk::RGB::named(rltk::BLACK)
-        })
-        .with(Enemy {})
-        .with(Viewshed {
-            visible_tiles: Vec::new(),
-            range: 5,
-            dirty: true
-        })
-        .with(Facing {direction: Direction::Up})
-        .with(BlocksTile {})
-        .with(HumanoidBody::new(20))
-        .with(Inventory {items: EntityVec::new()})
-        .with(Name {value: "Goon".to_string()})
-        .marked::<SimpleMarker<SerializeMarker>>()
-        .build();
-
-    let (gun_x, gun_y) = (player_x, player_y);
-    game_state.ecs
-        .create_entity()
-        .with(Position {
-            x: gun_x,
-            y: gun_y
-        })
-        .with(Renderable {
-            glyph: 169,
-            color: rltk::RGB::named(rltk::BLUE),
-            background: rltk::RGB::named(rltk::BLACK)
-        })
-        .with(Gettable {})
-        .with(Name {value: "Tommygun".to_string()})
-        .with(Firearm {range: 52})
-        .with(Equippable {
-            equipped: false,
-            slot: ItemSlot::MainWeapon
-        })
-        .marked::<SimpleMarker<SerializeMarker>>()
-        .build();
-    game_state.ecs
-        .create_entity()
-        .with(Position {
-            x: gun_x,
-            y: gun_y + 1
-        })
-        .with(Renderable {
-            glyph: 169,
-            color: rltk::RGB::named(rltk::BLUE),
-            background: rltk::RGB::named(rltk::BLACK)
-        })
-        .with(Gettable {})
-        .with(Name {value: "M1911".to_string()})
-        .with(Firearm {range: 52})
-        .with(Equippable {
-            equipped: false,
-            slot: ItemSlot::MainWeapon
-        })
-        .marked::<SimpleMarker<SerializeMarker>>()
-        .build();
-    game_state.ecs
-        .create_entity()
-        .with(Position {
-            x: gun_x - 1,
-            y: gun_y
-        })
-        .with(Renderable {
-            glyph: 30,
-            color: rltk::RGB::named(rltk::BLUE),
-            background: rltk::RGB::named(rltk::BLACK)
-        })
-        .with(Gettable {})
-        .with(Name {value: "Fedora".to_string()})
-        .with(Equippable {
-            equipped:false,
-            slot: ItemSlot::Head
-        })
-        .marked::<SimpleMarker<SerializeMarker>>()
-        .build();
-    game_state.ecs
-        .create_entity()
-        .with(Position {
-            x: gun_x + 1,
-            y: gun_y
-        })
-        .with(Renderable {
-            glyph: 239,
-            color: rltk::RGB::named(rltk::BLUE),
-            background: rltk::RGB::named(rltk::BLACK)
-        })
-        .with(Gettable {})
-        .with(Name {value: "Trenchcoat".to_string()})
-        .with(Equippable {
-            equipped: false,
-            slot: ItemSlot::Torso
-        })
-        .marked::<SimpleMarker<SerializeMarker>>()
-        .build();
-
-    let (tank_x, tank_y) = map.rooms[3].center();
-    game_state.ecs
-        .create_entity()
-        .with(Position {
-            x: tank_x,
-            y: tank_y
-        })
-        .with(Size {
-            x: 3,
-            y: 3
-        })
-        .with(LargeRenderable {
-            glyphs: vec![213,
-                        rltk::to_cp437('│'),
-                        rltk::to_cp437('╕'),
-                        rltk::to_cp437('╞'),
-                        rltk::to_cp437('█'),
-                        rltk::to_cp437('╡'),
-                        rltk::to_cp437('╘'),
-                        rltk::to_cp437('═'),
-                        rltk::to_cp437('╛')],
-            color: rltk::RGB::named(rltk::RED),
-            background: rltk::RGB::named(rltk::BLACK)
-        })
-        .with(Viewshed {
-            visible_tiles: Vec::new(),
-            range: 5,
-            dirty: true
-        })
-        .with(BlocksTile {})
-        .with(Facing {direction: Direction::Up})
-        .with(Vehicle {})
-        .with(Name {value: "Tank".to_string()})
-        .marked::<SimpleMarker<SerializeMarker>>()
-        .build();
+    // let player_entity = game_state.ecs
+    //     .create_entity()
+    //     .with(Position {
+    //         x: player_x,
+    //         y: player_y
+    //     })
+    //     .with(Renderable {
+    //         glyph: rltk::to_cp437('8'),
+    //         color: rltk::RGB::named(rltk::YELLOW),
+    //         background: rltk::RGB::named(rltk::BLACK)
+    //     })
+    //     .with(Player {})
+    //     .with(Viewshed {
+    //         visible_tiles: Vec::new(),
+    //         range: 10,
+    //         dirty: true
+    //     })
+    //     .with(Facing {direction: Direction::Up})
+    //     .with(HumanoidBody::new(20))
+    //     .with(Inventory {items: EntityVec::new()})
+    //     .with(Name {value: "Player".to_string()})
+    //     .marked::<SimpleMarker<SerializeMarker>>()
+    //     .build();
+    // game_state.ecs.insert(player_entity);
 
     let cursor_pos = Point{x:0, y:0};
 
-    game_state.ecs.insert(map);
-    game_state.ecs.insert(cursor_pos);
-    game_state.ecs.insert(RunState::PreRun);
-    game_state.ecs.insert(GameLog {entries: vec!["Welcome!".to_string()]});
-    game_state.ecs.insert(rex_assets::RexAssets::new());
-
+    game_state.log.entries.push("Welcome!".to_string());
+ 
     rltk::main_loop(context, game_state)
 }
--- src/state.rs
+++ src/state.rs
@@ -1,5 +1,4 @@
 use rltk::{Rltk, GameState, Point, console};
-use specs::prelude::*;
 use super::*;
 use std::time::{Instant};
 
@@ -17,10 +16,11 @@ pub enum RunState {
 }
 
 pub struct State {
-    pub ecs: World,
+    pub run_state: RunState,
     pub mouse_pos: Point,
-    pub menu_stack: Vec<Menu>,
-    pub inventory_screen_selection: i32,
+    pub log: GameLog,
+//    pub menu_stack: Vec<Menu>,
+//    pub inventory_screen_selection: i32,
 
     last_tick: Instant,
 }
@@ -28,107 +28,33 @@ pub struct State {
 impl State {
     pub fn new() -> Self {
         Self {
-            ecs: World::new(),
-            last_tick: Instant::now(),
+            run_state: RunState::PreRun,
             mouse_pos: Point {x: 0, y:0},
-            menu_stack: Vec::new(),
-            inventory_screen_selection: 0
+            log: GameLog {entries: vec![]},
+            last_tick: Instant::now(),
         }
     }
 
-    fn run_systems(&mut self) {
-        let mut vis = VisibilitySystem {};
-        vis.run_now(&self.ecs);
-        let mut mapindex = MapIndexingSystem {};
-        mapindex.run_now(&self.ecs);
-        let mut enemy_ai = EnemyAI {};
-        enemy_ai.run_now(&self.ecs);
-        let mut tank_ai = TankAI {};
-        tank_ai.run_now(&self.ecs);
-        let mut inventory_system = InventorySystem {};
-        inventory_system.run_now(&self.ecs);
-        let mut damage_system = DamageSystem {};
-        damage_system.run_now(&self.ecs);
+    /// Moves the state machine forward.
+    fn execute(&mut self) {
     }
 }
 
 impl GameState for State {
+    /// Called periodically as real time advances.
     fn tick(&mut self, context: &mut Rltk) {
         let begin = Instant::now();
         
         context.cls();
 
-        let mut new_run_state;
-        {
-            let run_state = self.ecs.fetch::<RunState>();
-            new_run_state = *run_state;
-        }
-        match new_run_state {
+        match self.run_state {
             RunState::PreRun => {
-                self.run_systems();
-                new_run_state = RunState::AwaitingInput;
-                draw_main_screen(self, context);
+                self.execute();
+                self.run_state = RunState::AwaitingInput;
             },
-            RunState::AwaitingInput => {
-                new_run_state = main_screen_input(self, context);
-                draw_main_screen(self, context);
-            },
-            RunState::MenuInput => {
-                new_run_state = menu_input(self, context);
-                draw_main_screen(self, context);
-            },
-            RunState::TargetingInput => {
-                new_run_state = targeting_input(self, context);
-                draw_main_screen(self, context);
-            }
-            RunState::PlayerTurn => {
-                self.run_systems();
-                new_run_state = RunState::EnemyTurn;
-                draw_main_screen(self, context);
-            },
-            RunState::EnemyTurn => {
-                self.run_systems();
-                new_run_state = RunState::AwaitingInput;
-                draw_main_screen(self, context);
-            },
-            RunState::Saving => {
-                {
-                    let result = saveload_system::save_game(&mut self.ecs);
-                    let mut game_log = self.ecs.fetch_mut::<GameLog>();
-                    match result {
-                        Ok(_) => game_log.entries.push("Game saved.".to_string()),
-                        Err(_) => game_log.entries.push("Game could not be saved.".to_string())
-                    }
-                    new_run_state = RunState::AwaitingInput;
-                }
-                draw_main_screen(self, context);
-            },
-            RunState::Loading => {
-                {
-                    let result = saveload_system::load_game(&mut self.ecs);
-                    let mut game_log = self.ecs.fetch_mut::<GameLog>();
-                    match result {
-                        Ok(_) => game_log.entries.push("Game loaded.".to_string()),
-                        Err(_) => game_log.entries.push("Game could not be loaded.".to_string())
-                    }
-                    new_run_state = RunState::AwaitingInput;
-                }
-                draw_main_screen(self, context);
-            },
-            RunState::InventoryScreen => {
-                new_run_state = inventory_screen_input(self, context);
-                draw_inventory_screen(self, context);
-            }
+            _ => ()
         }
-        {
-            let mut run_writer = self.ecs.write_resource::<RunState>();
-            *run_writer = new_run_state;
-        }
-
-        if new_run_state == RunState::MenuInput {
-            draw_menu(self, context);
-        }
-
+ 
         let tick_time = begin.elapsed().as_micros();
         if tick_time > 6000 {
             console::log(format!("Tick time: {}", tick_time));
