a65d891b72b4f96c43eedc136eefdf8af6e765e1
Johan Larsson <hnefi.mail@gmail.com>
2026-07-25 11:43:26 +0200

    Add convenience function for directions

--- src/components.rs
+++ src/components.rs
@@ -53,6 +53,19 @@ impl Direction {
             Direction::UpLeft    => "northwest",
         }
     }
+
+    pub fn delta_pos(&self) -> (i32, i32) {
+        match self {
+            Direction::Up        => ( 0, -1),
+            Direction::UpRight   => ( 1, -1),
+            Direction::Right     => ( 1,  0),
+            Direction::DownRight => ( 1,  1),
+            Direction::Down      => ( 0,  1),
+            Direction::DownLeft  => (-1,  1),
+            Direction::Left      => (-1,  0),
+            Direction::UpLeft    => (-1, -1),
+        }
+    }
 }
 
 #[derive (Copy, Clone)]
--- src/entity.rs
+++ src/entity.rs
@@ -356,16 +356,7 @@ impl Entity {
 }
 
 fn forward_intent(pos: Point, facing: Direction) -> Intent {
-    let (dx, dy): (i32, i32) = match facing {
-        Direction::Up        => ( 0, -1),
-        Direction::UpRight   => ( 1, -1),
-        Direction::Right     => ( 1,  0),
-        Direction::DownRight => ( 1,  1),
-        Direction::Down      => ( 0,  1),
-        Direction::DownLeft  => (-1,  1),
-        Direction::Left      => (-1,  0),
-        Direction::UpLeft    => (-1, -1),
-    };
+    let (dx, dy): (i32, i32) = facing.delta_pos();
     Intent {
         phase: ExecutionPhase::Movement,
         data: IntentData::Target(Point { x: pos.x + dx, y: pos.y + dy }),
