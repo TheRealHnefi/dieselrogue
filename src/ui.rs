e78dad72462b8f89c4697d6dc4512660d9491dd7
Johan Larsson <hnefi.mail@gmail.com>
2026-07-25 11:43:20 +0200

    Add basic animation system.

--- src/ui.rs
+++ src/ui.rs
@@ -1,8 +1,9 @@
-use rltk::{RGB, Rltk, Point};
+use rltk::{RGB, Rltk};
 use std::cmp::max;
+use crate::components::*;
 use crate::state::*;
 use crate::map::*;
-use crate::components::*;
+use crate::Rect;
 
 pub const SCREEN_WIDTH: usize = 80;
 pub const SCREEN_HEIGHT: usize = 50;
@@ -17,35 +18,18 @@ pub fn draw_menu(state: &State, context: &mut Rltk, monotime: u128) {
 }
 
 pub fn draw_main_screen(state: &mut State, context: &mut Rltk, monotime: u128) {
-    let camera_pos = match state.world.get_player() {
-        Ok(player) => player.position,
-        Err(_) => Point{x: (SCREEN_WIDTH / 2) as i32, y: (SCREEN_HEIGHT / 2) as i32}
-    };
-
-    let mut top = max(camera_pos.y - SCREEN_HEIGHT as i32 / 2, 0);
-    let mut left = max(camera_pos.x - SCREEN_WIDTH as i32 / 2, 0);
-    let mut bottom = top + VIEWPORT_HEIGHT as i32;
-    let mut right = left + SCREEN_WIDTH as i32;
-
-    if right > state.world.map.width as i32{
-        right = state.world.map.width as i32;
-        left = right - SCREEN_WIDTH as i32;
-    }
-    if bottom > state.world.map.height as i32 {
-        bottom = state.world.map.height as i32;
-        top = bottom - VIEWPORT_HEIGHT as i32;
-    }
-
     let blink = (monotime / 250) % 2 == 0;
 
-    draw_map(&state.world.map, left, right, top, bottom, context, blink);
-    draw_main_ui(state, left, right, top, bottom, context, blink);
+    let viewport = state.get_viewport();
+
+    draw_map(&state.world.map, viewport, context, blink);
+    draw_main_ui(state, viewport, context, blink);
 }
 
-fn draw_main_ui(state: &mut State, left: i32, _right: i32, top: i32, _bottom: i32, context: &mut Rltk, blink: bool) {
+fn draw_main_ui(state: &mut State, viewport: Rect, context: &mut Rltk, blink: bool) {
     context.draw_box(0, VIEWPORT_HEIGHT, SCREEN_WIDTH - 1, BOTTOM_BAR_HEIGHT - 1, RGB::named(rltk::WHITE), RGB::named(rltk::BLACK));
     if blink && state.run_state == RunState::AwaitingPositionalTargetingInput {
-        context.set_bg(state.cursor_pos.x - left, state.cursor_pos.y - top, RGB::named(rltk::PINK));
+        context.set_bg(state.cursor_pos.x - viewport.x1, state.cursor_pos.y - viewport.y1, RGB::named(rltk::PINK));
     }
 
     let mut y = SCREEN_HEIGHT - BOTTOM_BAR_HEIGHT + 1;
@@ -56,15 +40,15 @@ fn draw_main_ui(state: &mut State, left: i32, _right: i32, top: i32, _bottom: i3
     }
 }
 
-fn draw_map(map: &Map, left: i32, right: i32, top: i32, bottom: i32, ctx: &mut Rltk, blink: bool) {
+fn draw_map(map: &Map, viewport: Rect, ctx: &mut Rltk, blink: bool) {
     let mut x = 0;
     let mut y = 0;
     for (idx, tile) in map.tiles.iter().enumerate() {
         let tile_pos = map.idx_pos(idx);
-        if tile_pos.x < left
-            || tile_pos.x >= right 
-            || tile_pos.y < top
-            || tile_pos.y >= bottom {
+        if tile_pos.x < viewport.x1
+            || tile_pos.x >= viewport.x2
+            || tile_pos.y < viewport.y1
+            || tile_pos.y >= viewport.y2 {
             continue;
         }
         if map.revealed_tiles[idx] {
@@ -88,7 +72,7 @@ fn draw_map(map: &Map, left: i32, right: i32, top: i32, bottom: i32, ctx: &mut R
             ctx.set(x, y, renderable.color, renderable.background, renderable.glyph);
         }
         x += 1;
-        if x >= right - left {
+        if x >= viewport.x2 - viewport.x1 {
             x = 0;
             y += 1;
         }
