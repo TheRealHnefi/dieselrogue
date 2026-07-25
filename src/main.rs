e93741bf3698396d6f18583b040ba2183b938a1f
Johan Larsson <hnefi.mail@gmail.com>
2026-07-25 11:41:35 +0200

    Add big items exemplified by tank.

--- src/main.rs
+++ src/main.rs
@@ -27,18 +27,25 @@ pub use inventory_system::*;
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
 
-    let context = RltkBuilder::simple80x50()
+    let mut context = RltkBuilder::simple80x50()
         .with_title("Diesel Rogue")
+        .with_resource_path("resources")
+        .with_font("rexpaint_cp437_10x10.png", 10, 10)
+        .with_tile_dimensions(10, 10)
         .build()?;
     
+    context.set_active_font(1, true);
+
     let mut game_state = State::new();
 
     game_state.ecs.register::<Player>();
     game_state.ecs.register::<Enemy>();
     game_state.ecs.register::<Position>();
+    game_state.ecs.register::<Size>();
     game_state.ecs.register::<Direction>();
     game_state.ecs.register::<Facing>();
     game_state.ecs.register::<Renderable>();
+    game_state.ecs.register::<LargeRenderable>();
     game_state.ecs.register::<Viewshed>();
     game_state.ecs.register::<Name>();
     game_state.ecs.register::<BlocksTile>();
@@ -57,7 +64,7 @@ fn main() -> rltk::BError {
             y: player_y
         })
         .with(Renderable {
-            glyph: rltk::to_cp437('@'),
+            glyph: rltk::to_cp437('8'),
             color: rltk::RGB::named(rltk::YELLOW),
             background: rltk::RGB::named(rltk::BLACK)
         })
@@ -98,21 +105,54 @@ fn main() -> rltk::BError {
         .with(Name {value: "Goon".to_string()})
         .build();
 
-        let (gun_x, gun_y) = map.rooms[2].center();
-        game_state.ecs
-            .create_entity()
-            .with(Position {
-                x: gun_x,
-                y: gun_y
-            })
-            .with(Renderable {
-                glyph: 169,
-                color: rltk::RGB::named(rltk::BLUE),
-                background: rltk::RGB::named(rltk::BLACK)
-            })
-            .with(GettableItem {})
-            .with(Name {value: "Gun".to_string()})
-            .build();
+    let (gun_x, gun_y) = map.rooms[2].center();
+    game_state.ecs
+        .create_entity()
+        .with(Position {
+            x: gun_x,
+            y: gun_y
+        })
+        .with(Renderable {
+            glyph: 169,
+            color: rltk::RGB::named(rltk::BLUE),
+            background: rltk::RGB::named(rltk::BLACK)
+        })
+        .with(GettableItem {})
+        .with(Name {value: "Gun".to_string()})
+        .build();
+
+    let (tank_x, tank_y) = map.rooms[3].center();
+    game_state.ecs
+        .create_entity()
+        .with(Position {
+            x: tank_x,
+            y: tank_y
+        })
+        .with(Size {
+            x: 3,
+            y: 3
+        })
+        .with(LargeRenderable {
+            glyphs: vec![213,
+                        rltk::to_cp437('│'),
+                        rltk::to_cp437('╕'),
+                        rltk::to_cp437('╞'),
+                        rltk::to_cp437('█'),
+                        rltk::to_cp437('╡'),
+                        rltk::to_cp437('╘'),
+                        rltk::to_cp437('═'),
+                        rltk::to_cp437('╛')],
+            color: rltk::RGB::named(rltk::RED),
+            background: rltk::RGB::named(rltk::BLACK)
+        })
+        .with(Viewshed {
+            visible_tiles: Vec::new(),
+            range: 5,
+            dirty: true
+        })
+        .with(Facing {direction: Direction::UP})
+        .with(Name {value: "Tank".to_string()})
+        .build();
 
     let cursor_pos = Point{x:0, y:0};
 
